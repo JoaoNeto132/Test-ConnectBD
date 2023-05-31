205c039dee17feb067a8cd2248c72b8cd16121c8
JoaoNeto132 <joaomasp12@gmail.com>
2023-05-31 16:03:13 -0300

    Criando tables

---
+++ CrudSimples/Models/Produto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CrudSimples.Models
+{
+    [Table("Produto")]
+    public class Produto
+    {
+        [Column("Id")]
+        [Display(Name = "Código")]
+
+        public int Id { get; set; }
+
+        [Column("Nome")]
+        [Display(Name = "Nome")]
+
+        public string Nome { get; set; }
+    }
+}
